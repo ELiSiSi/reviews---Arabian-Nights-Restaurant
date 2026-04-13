d0ec694908578f68ffbcf4b8226b3e6515d6d2f2
Ahmed Rizq <arezk0550@gmail.com>
2026-04-13 17:21:50 +0200

    first commit

--- app.js
+++ app.js
@@ -54,16 +54,27 @@ app.get('/', async (req, res) => {
 });
 
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}/rating`);
+
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+      return mongoose;
     });
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+  }
+
+  
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 
   export default app;
